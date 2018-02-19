15f77aa634fd0890e34deeeceb26c92113b41637
manojmay <31890386+manojmay@users.noreply.github.com>
2018-02-19 21:34:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,16 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/article -one', function(req, res){
+    res.send('article-one will be served ');
+});
+app.get('article-two',function(req,res){
+    res.send('article-two will served');
+});
+app.get('/article-3',function(req,res){
+    res.send('article -3 will be served');
+});
+
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
